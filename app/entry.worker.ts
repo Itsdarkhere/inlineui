5e8e8742f9c809f7876f4a5ce7c1b88693841fae
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2026-02-08 11:58:41 +0200

    Worker fix

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -7,16 +7,24 @@ declare module "react-router" {
     cloudflare: {
       env: Env;
       ctx: ExecutionContext;
+      cf: Request["cf"];
     };
   }
 }
 
-const requestHandler = createRequestHandler(build, "production");
+const requestHandler = createRequestHandler({
+  build,
+  mode: "production",
+  getLoadContext: ({ context }) => context,
+});
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-    return requestHandler(request, {
-      cloudflare: { env, ctx },
+    return requestHandler({
+      request,
+      env,
+      waitUntil: ctx.waitUntil.bind(ctx),
+      passThroughOnException: ctx.passThroughOnException.bind(ctx),
     });
   },
 } satisfies ExportedHandler<Env>;
